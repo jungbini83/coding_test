166e2228c5330d34299a66357ae68067683f9438
jungbini83 <jungbini83@gmail.com>
2025-05-19 12:24:49 +0000

    feat: Implement basic MyApp widget (Step 2)
    This commit introduces the basic structure for the MyApp widget, representing the second step in the tutorial. The initial empty MyApp widget (Step 1) is commented out.

--- lib/ch2_1_2_text_widget.dart
+++ lib/ch2_1_2_text_widget.dart
@@ -3,10 +3,13 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 // 1단계
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
 
-  Widget build(BuildContext context) {
-    return MaterialApp();
-  }
-}
+//   Widget build(BuildContext context) {
+//     return MaterialApp();
+//   }
+// }
+
+// 2단계
+class MyApp extends StatelessWidget
