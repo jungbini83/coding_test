f600119f62689cdb1bea98c1afde6530f67b944e
jungbini83 <jungbini@gmail.com>
2025-05-19 05:14:33 +0000

    feat: Implement basic 'Hello, World!' app in Flutter
    This commit introduces a basic Flutter app that displays 'Hello, World!' in the center of the screen.

--- lib/ch1_1_1_FlutterBasic1.dart
+++ lib/ch1_1_1_FlutterBasic1.dart
@@ -1,1 +1,13 @@
 import 'package:flutter/material.dart;'
+
+
+import 'package:flutter/widgets.dart';void main() {
+  runApp(   
+    const Center(    
+      child: Text (  
+        'Hello, World!',
+        TextDirection: Text
+      )
+    )
+  )
+}
