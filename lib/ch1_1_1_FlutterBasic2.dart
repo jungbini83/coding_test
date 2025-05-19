3189fbde08d8998c984cd21a642b5454fecf6d89
jungbini83 <jungbini@gmail.com>
2025-05-19 11:52:48 +0000

    Updated Changes

--- lib/ch1_1_1_FlutterBasic2.dart
+++ lib/ch1_1_1_FlutterBasic2.dart
@@ -1,3 +1,14 @@
 import 'package:flutter/material.dart';
 
-main() => runApp(My)
+main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(  
+      
+    )
+  }
+}
